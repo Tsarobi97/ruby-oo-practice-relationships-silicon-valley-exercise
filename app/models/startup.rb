0aac318bed03bfc2c6cc31e5bb2ac85040ed69c2
RobertObi <robobi97@googlemail.com>
2021-03-02 19:20:22 -0600

    commit tree 187590e51557b0ea26a0b78b7980f6bae94fe677

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,26 @@
 class Startup
 
+    attr_writer :domain, :name
+    attr_reader :founder
+
+    @@all = []
+
+def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain  domain
+    @@all << self
+end
+
+def pivot(new_domain,new_name)
+    self.domain = domain
+    self.name = name
+end 
+
+
+def self.all
+    @@all 
+end 
+
+
 end
